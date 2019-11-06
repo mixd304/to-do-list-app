259b5591922b7f0cf09c6ec0601769bd96b5b6d9
Darius Mix <d.mix@dvz-mv.de>
2019-11-06 15:13:22 +0100

    Tabelle erstellt

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task';
 
 @Component({
   selector: 'app-task-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+  tasks: Task[] = [{
+    checked: false,
+    description: 'Die Toilette reinigen',
+    tags: ['Klo', 'Haushalt', 'WC'],
+    title: 'Klo sauber machen'
+  }, {
+    checked: false,
+    description: 'Die Toilette reinigen 2 ',
+    tags: ['Klo', 'Haushalt', 'WC'],
+    title: 'Klo sauber machen 2'
+  }];
 
   constructor() { }
 
