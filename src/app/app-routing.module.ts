b953dd1146986894663d34e1397c09735bf5a66b
Tim Vinzing <t.vinzing@dvz-mv.de>
2019-11-06 14:00:02 +0100

    Task-List Komponente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TaskListComponent } from "./task-list/task-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'table', component: TaskListComponent},
+  {path: '', redirectTo: 'table', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
