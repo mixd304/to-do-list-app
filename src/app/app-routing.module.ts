ef6e92b9413a4b821366ce695a081d8970f2b1a5
Tim Vinzing <t.vinzing@dvz-mv.de>
2019-11-07 08:17:16 +0100

    Check und delete Buttons (Ohne Funktion)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path: 'table', component: TaskListComponent},
+  {path: 'details', component: TaskDetailsComponent},
+  {path: 'test', component: TestComponent},
   {path: '', redirectTo: 'table', pathMatch: 'full'}
 ];
 
