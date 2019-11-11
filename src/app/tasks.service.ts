243c72e36adebd7df03d1c27b9a39d8a6542d175
Tim Vinzing <t.vinzing@dvz-mv.de>
2019-11-11 10:34:30 +0100

    nochmehr toten Code

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
 
 import { Task } from './task';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-  private tasksUrl = 'api/tasks';
   private jsonObj: string;
   private tasks: Task[] = [];
-
-  private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
 
   constructor(
     private http: HttpClient,
@@ -41,7 +34,7 @@ export class TasksService {
   addTask(task: Task): void {
     task.id = localStorage.length + 1;
     this.jsonObj = JSON.stringify(task);
-    console.log("Title: " + task.title + ", ID: " + task.id );
+    console.log('Title: ' + task.title + ', ID: ' + task.id );
     localStorage.setItem('' + task.id, this.jsonObj);
   }
 
@@ -50,21 +43,11 @@ export class TasksService {
     this.tasks = [];
 
     for (i = 1; i <= localStorage.length; i++) {
-      let task: Task = JSON.parse(localStorage.getItem('' + i)) as Task;
-      if(task.title.match(".*"+term+".*")) {
+      const task: Task = JSON.parse(localStorage.getItem('' + i)) as Task;
+      if (task.title.match('.*' + term + '.*')) {
         this.tasks.push(task);
       }
     }
     return this.tasks;
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      console.error(error); // log to console instead
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 }
