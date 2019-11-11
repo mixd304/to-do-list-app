4bcf78de7e7170544ec0471a1a0cb73f59b3a829
Darius Mix <d.mix@dvz-mv.de>
2019-11-11 11:24:33 +0100

    ID-System überarbeitet

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -25,27 +25,51 @@ export class TasksService {
   getTasks(): Task[] {
     let i;
     this.tasks = [];
-    for (i = 1; i <= localStorage.length; i++) {
-      this.tasks.push(JSON.parse(localStorage.getItem('' + i)) as Task);
+    for (i = 1; i <= (localStorage.getItem('0') as unknown as number); i++) {
+      const task: Task = JSON.parse(localStorage.getItem('' + i)) as Task;
+      console.log(task);
+
+      if(task == null) {
+        console.log('kaputt');
+      } else {
+        console.log('nicht kaputt');
+        this.tasks.push(task);
+      }
     }
     return this.tasks;
   }
 
   addTask(task: Task): void {
-    task.id = localStorage.length + 1;
+    let anzahl: number = localStorage.getItem('0') as unknown as number;
+    if(anzahl == null) {
+      anzahl = 1;
+      localStorage.setItem('0', '1');
+    } else {
+      anzahl++;
+      localStorage.setItem('0', anzahl + "");
+    }
+
+    task.id = anzahl;
     this.jsonObj = JSON.stringify(task);
     console.log('Title: ' + task.title + ', ID: ' + task.id );
     localStorage.setItem('' + task.id, this.jsonObj);
   }
 
+  deleteTask(task: Task): void {
+    localStorage.removeItem(task.id + "");
+  }
+
   searchTask(term: string): Task[] {
     let i;
     this.tasks = [];
-
-    for (i = 1; i <= localStorage.length; i++) {
+    for (i = 1; i <= (localStorage.getItem('0') as unknown as number); i++) {
       const task: Task = JSON.parse(localStorage.getItem('' + i)) as Task;
-      if (task.title.match('.*' + term + '.*')) {
-        this.tasks.push(task);
+      if(task == null) {
+
+      } else {
+        if (task.title.match('.*' + term + '.*')) {
+          this.tasks.push(task);
+        }
       }
     }
     return this.tasks;
